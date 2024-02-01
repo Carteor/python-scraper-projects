59e8c91d33a0eb73f34fdefba326a9372dfcf144
Carteor <carteorcure@gmail.com>
2024-02-01 18:33:33 +0600

    scrape the current page for title, desc and url

--- indie-movies/indie-movies.py
+++ indie-movies/indie-movies.py
@@ -1,27 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+
+import math
+
+
+def scrape_page(driver):
+    movies_element = driver.find_elements(
+        By.CSS_SELECTOR,
+        '#__next > main > div.ipc-page-content-container.ipc-page-content-container--center.sc-a80fc520-0.kpyNQn > div.ipc-page-content-container.ipc-page-content-container--center > section > section > div > section > section > div:nth-child(2) > div > section > div.ipc-page-grid.ipc-page-grid--bias-left.ipc-page-grid__item.ipc-page-grid__item--span-2 > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > ul > li'
+    )
+
+    print(len(movies_element))
+
+    movies_list = []
+    for movie in movies_element:
+        title = movie.find_element(By.CSS_SELECTOR,
+                                   'h3.ipc-title__text').text
+        try:
+            description = movie.find_element(By.CSS_SELECTOR,
+                                             'div.ipc-html-content-inner-div').text
+        except NoSuchElementException:
+            description = 'Not available'
+
+        url = movie.find_element(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper').get_attribute('href')
+
+        movies_list.append({'title': title,
+                            'description': description,
+                            'url': url})
+
+    return movies_list
+
+
+def load_pages(page_number, driver):
+    print("load_pages()")
+    for i in range(2):
+        see_more_button = driver.find_element(By.CSS_SELECTOR,
+                                              'button.ipc-see-more__button')
+        ActionChains(driver).move_to_element(see_more_button).perform()
+
+
 
 url = 'https://www.imdb.com/search/title/?sort=release_date,desc&keywords=independent-film'
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(0.5)
+driver.implicitly_wait(1)
 driver.get(url)
 
 title = driver.title
 print(title)
 
-movies = driver.find_elements(
-    By.CSS_SELECTOR,
-    '#__next > main > div.ipc-page-content-container.ipc-page-content-container--center.sc-a80fc520-0.kpyNQn > div.ipc-page-content-container.ipc-page-content-container--center > section > section > div > section > section > div:nth-child(2) > div > section > div.ipc-page-grid.ipc-page-grid--bias-left.ipc-page-grid__item.ipc-page-grid__item--span-2 > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > ul > li'
-)
+page_number_element = driver.find_element(By.XPATH,
+                                          '//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/div[1]')
+page_number = math.ceil(int(page_number_element.text.split()[2].replace(',', '')) / 50)
+print(page_number)
 
-print(len(movies))
+# load_pages(2, driver)
 
-for movie in movies:
-    #title
-    title = movie.find_element(By.CSS_SELECTOR,
-                               '#__next > main > div.ipc-page-content-container.ipc-page-content-container--center.sc-a80fc520-0.kpyNQn > div.ipc-page-content-container.ipc-page-content-container--center > section > section > div > section > section > div:nth-child(2) > div > section > div.ipc-page-grid.ipc-page-grid--bias-left.ipc-page-grid__item.ipc-page-grid__item--span-2 > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > div > div.sc-73c670dc-3.cFICGu > div.sc-1e00898e-0.jyXHpt > div > a > h3')
-    #description
-    #url
+movie_list = scrape_page(driver)
+print(movie_list)
 
 driver.quit()
