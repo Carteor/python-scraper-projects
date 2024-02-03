0ccf19dba13d9820560ce50c955abf40bde5b16c
Carteor <carteorcure@gmail.com>
2024-02-03 20:01:34 +0600

    finish the script

--- indie-movies/indie-movies.py
+++ indie-movies/indie-movies.py
@@ -4,71 +4,76 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 import math
 import time
+import pandas as pd
 
-
-def scrape_details(driver, movie_url='https://www.imdb.com/title/tt0335345/'):
-    driver.get(movie_url)
-
-    photos_section = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.CSS_SELECTOR,
-             'section[data-testid="Photos"]')
+def scrape_details(driver):
+    try:
+        element_to_scroll = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (
+                By.CSS_SELECTOR,
+                'div[data-testid="storyline-header"]'
+                )
+            )
         )
-    )
-    driver.execute_script("arguments[0].scrollIntoView();",
-                          photos_section)
+        driver.execute_script("arguments[0].scrollIntoView(true);", element_to_scroll)
 
-    # Scroll to Storyline element to start loading remaining data
-    storyline_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.CSS_SELECTOR,
-             '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-304f99f6-0.fSJiHR > div > section > div > div.sc-a83bf66d-1.gYStnb.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(25) > div.ipc-title.ipc-title--base.ipc-title--section-title.ipc-title--on-textPrimary > div > hgroup > h3 > span')
-        )
-    )
-
-
-    # Use Javascript to avoid bugs
-    driver.execute_script("arguments[0].scrollIntoView();",
-                          storyline_element)
-    # Take a break to load the element
-    time.sleep(5)
+        time.sleep(1)
+    except TimeoutException:
+        print("Project in development")
 
     #  Genres
-    genres_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH,
-             '//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[6]/div[2]/ul[2]/li[2]/div/ul')
+    try:
+        genres_ul_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH,
+                 '//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[4]/div[2]/ul[2]/li[1]/div/ul')
+            )
         )
-    )
-    genres = genres_element.text
-    # print(genres)
+        genres = genres_ul_element.text
+    except TimeoutException:
+        genres = 'No genres available'
 
     #  Release Year
-    release_year_element = driver.find_element(By.XPATH,
-                                               '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[1]/a')
-    release_year = release_year_element.text
+    try:
+        release_year_element = driver.find_element(By.XPATH,
+                                                   '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[1]/a')
+        release_year = release_year_element.text
+    except NoSuchElementException:
+        release_year = 'No release year available'
+
     # print(release_year)
 
     #  Ratings
-    ratings_element = driver.find_element(
-        By.CSS_SELECTOR,
-        'div[data-testid="hero-rating-bar__aggregate-rating__score"]')
-    ratings = ratings_element.text.replace('\n', '')
+    try:
+        ratings_element = driver.find_element(
+            By.CSS_SELECTOR,
+            'div[data-testid="hero-rating-bar__aggregate-rating__score"]')
+        ratings = ratings_element.text.replace('\n', '')
+    except NoSuchElementException:
+        ratings = 'No ratings available'
     # print(f'ratings: \'{ratings.strip()}\'')
 
     #  Box Office
-    box_office_element = driver.find_element(By.XPATH,
-                                             '//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[4]/div/ul/li/span')
-    box_office = box_office_element.text
+    try:
+        box_office_element = driver.find_element(By.XPATH,
+                                                 '//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[4]/div/ul/li/span')
+        box_office = box_office_element.text
+    except NoSuchElementException:
+        box_office = 'No box office available'
     # print(box_office)
 
     # #  YouTube Trailer Link
-    trailer_link_element = driver.find_element(By.XPATH,
-                                               '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div[2]/div[2]/a[2]')
-    trailer_link = trailer_link_element.get_attribute('href')
+    try:
+        trailer_link_element = driver.find_element(By.XPATH,
+                                                   '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div[2]/div[2]/a[2]')
+        trailer_link = trailer_link_element.get_attribute('href')
+    except NoSuchElementException:
+        trailer_link = 'No trailer link available'
     # print(trailer_link)
 
     return {
@@ -89,6 +94,9 @@ def scrape_page(driver):
     print(len(movies_element))
 
     movies_list = []
+    # columns = ['title', 'description', 'url', 'genres:', 'release_year', 'ratings', 'box_office', 'trailer_link']
+    # df = pd.DataFrame(columns=columns)
+
     for movie in movies_element:
         title = movie.find_element(By.CSS_SELECTOR,
                                    'h3.ipc-title__text').text
@@ -105,11 +113,21 @@ def scrape_page(driver):
             'description': description,
             'url': url}
 
-        y = scrape_details(driver, url)
+        driver.execute_script("window.open('{}', '_blank');".format(url))
+        driver.switch_to.window(driver.window_handles[-1])
+
+        y = scrape_details(driver)
+
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
         z = {**x, **y}
 
         movies_list.append(z)
+        print(z)
+
+        df = pd.DataFrame(movies_list)
+        df.to_csv('movies.csv', mode='w', header=True, index=False)
 
     return movies_list
 
@@ -130,7 +148,7 @@ def load_pages(page_number, driver):
 
 
 url = 'https://www.imdb.com/search/title/?sort=release_date,desc&keywords=independent-film'
-
+# url = 'https://www.imdb.com/title/tt5765844/'
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 
@@ -143,8 +161,6 @@ page_number_element = driver.find_element(By.XPATH,
                                           '//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/div[1]')
 page_number = math.ceil(int(page_number_element.text.split()[2].replace(',', '')) / 50)
 print(page_number)
-
-# load_pages(2, driver)
 
 movie_list = scrape_page(driver)
 print(movie_list)
