a8e45fa722a3b594ce3dd963dd26c968e796a72e
Carteor <carteorcure@gmail.com>
2024-02-07 17:58:03 +0600

    implement a function to scrape the details panel

---
+++ google-maps/google_maps.py
@@ -0,0 +1,100 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+
+def scrape_panel(listing):
+    listing.click()
+
+    time.sleep(5)
+
+    panel_element = driver.find_element(
+        By.CSS_SELECTOR,
+        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc'
+    )
+
+    heading_element = panel_element.find_element(
+        By.CSS_SELECTOR,
+        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div.TIHn2 > div > div.lMbq3e > div:nth-child(1) > h1'
+    )
+    # print(heading_element.text)
+    # Name Of Business
+    name = heading_element.text
+    # Address Of Business
+    address = panel_element.find_element(
+        By.CSS_SELECTOR,
+        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(3) > button > div > div.rogA2c > div.Io6YTe.fontBodyMedium.kR99db'
+    ).text
+    # print(address)
+    # Phone Number
+    phone_number = panel_element.find_element(
+        By.CSS_SELECTOR,
+        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(7) > button > div > div.rogA2c > div.Io6YTe.fontBodyMedium.kR99db'
+    ).text
+    # print(phone_number)
+    # Email - businesses don't have emails?
+    # try:
+    #     email = panel_element.find_element(
+    #     By.CSS_SELECTOR,
+    #     ''
+    # ).text
+    # except NoSuchElementException:
+    #     email = 'No email available'
+
+    # Website
+    try:
+        website = panel_element.find_element(
+        By.CSS_SELECTOR,
+        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(7) > a > div > div.rogA2c.ITvuef > div.Io6YTe.fontBodyMedium.kR99db'
+    ).text
+    except NoSuchElementException:
+        website = 'No website available'
+    # print(website)
+
+    return {
+        'name': name,
+        'address': address,
+        'phone_number': phone_number,
+        #'email': email,
+        'website': website
+    }
+
+
+url = 'https://www.google.com/maps/'
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+driver.get(url)
+
+input_element = driver.find_element(
+    By.CSS_SELECTOR,
+    'input#searchboxinput'
+)
+
+location = ''
+business = 'dentist'
+
+input_element.send_keys(f'{location} {business}')
+input_element.send_keys(Keys.ENTER)
+
+time.sleep(10)
+
+listing = driver.find_element(
+    By.CSS_SELECTOR,
+    'a.hfpxzc'
+)
+
+ActionChains(driver).move_to_element(listing).perform()
+
+panel_data = scrape_panel(listing)
+
+print(panel_data)
+
+
