7c21b332308a87723d5380689b6abce7f228c427
Carteor <carteorcure@gmail.com>
2024-02-07 20:04:50 +0600

    finish implementing scraping with infinite scroll, dealing with Back To Top floating button bug, and saving to csv

--- google-maps/google_maps.py
+++ google-maps/google_maps.py
@@ -6,13 +6,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+
+import pandas as pd
 
 
 def scrape_panel(listing):
-    listing.click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(listing))
 
-    time.sleep(5)
+    try:
+        listing.click()
+    except ElementClickInterceptedException:
+        driver.execute_script("""
+        var button = document.querySelector('#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.RiRi5e.Hk4XGb.Yt0HSb > div > button');
+        if (button) {
+            button.remove();
+        }
+        """)
+        listing.click()
+
+    time.sleep(2)
 
     panel_element = driver.find_element(
         By.CSS_SELECTOR,
@@ -33,10 +46,14 @@ def scrape_panel(listing):
     ).text
     # print(address)
     # Phone Number
-    phone_number = panel_element.find_element(
-        By.CSS_SELECTOR,
-        '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(7) > button > div > div.rogA2c > div.Io6YTe.fontBodyMedium.kR99db'
-    ).text
+    try:
+        phone_number = panel_element.find_element(
+            By.CSS_SELECTOR,
+            '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(6) > button > div > div.rogA2c > div.Io6YTe.fontBodyMedium.kR99db'
+            # '#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div:nth-child(7) > div:nth-child(7) > button > div > div.rogA2c > div.Io6YTe.fontBodyMedium.kR99db'
+        ).text
+    except NoSuchElementException:
+        phone_number = 'No phone number available'
     # print(phone_number)
     # Email - businesses don't have emails?
     # try:
@@ -84,17 +101,39 @@ business = 'dentist'
 input_element.send_keys(f'{location} {business}')
 input_element.send_keys(Keys.ENTER)
 
-time.sleep(10)
+time.sleep(4)
 
-listing = driver.find_element(
-    By.CSS_SELECTOR,
-    'a.hfpxzc'
-)
+unique_labels = set()
+businesses_list = []
 
-ActionChains(driver).move_to_element(listing).perform()
+for i in range(20):
+    print(i)
 
-panel_data = scrape_panel(listing)
+    listings = driver.find_elements(
+        By.CSS_SELECTOR,
+        'a.hfpxzc'
+    )
 
-print(panel_data)
+    print(f'Number of listings: {len(listings)}')
 
+    for listing in listings:
+        aria_label = listing.get_attribute('aria-label')
 
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(listing))
+        ActionChains(driver).move_to_element(listing).perform()
+
+        if aria_label not in unique_labels:
+            unique_labels.add(aria_label)
+
+            panel_data = scrape_panel(listing)
+            businesses_list.append(panel_data)
+            print(panel_data)
+
+    for j in range(5):
+        main_panel = driver.find_element(By.CSS_SELECTOR, 'div[role="main"]')
+        ActionChains(driver).send_keys_to_element(main_panel, Keys.DOWN).perform()
+
+    time.sleep(1)
+
+df = pd.DataFrame(businesses_list)
+df.to_csv('output.csv', index=False)
